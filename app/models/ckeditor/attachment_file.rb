7082ca972311305b0f5b6905656d3869c06fc5d8
pacafs <paca.fs@gmail.com>
2017-06-06 17:41:49 +0100

    added aws support

--- app/models/ckeditor/attachment_file.rb
+++ app/models/ckeditor/attachment_file.rb
@@ -1,7 +1,25 @@
 class Ckeditor::AttachmentFile < Ckeditor::Asset
+  # has_attached_file :data,
+  #                   :url => "/ckeditor_assets/attachments/:id/:filename",
+  #                   :path => ":rails_root/public/ckeditor_assets/attachments/:id/:filename"
   has_attached_file :data,
-                    :url => "/ckeditor_assets/attachments/:id/:filename",
-                    :path => ":rails_root/public/ckeditor_assets/attachments/:id/:filename"
+
+      s3_credentials: {
+        access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+        bucket:            ENV['S3_BUCKET_NAME']
+      },
+
+      storage:        :s3,
+      s3_headers:     { "Cache-Control" => "max-age=31557600" },
+      s3_protocol:    "https",
+      bucket:         ENV['S3_BUCKET_NAME'],
+      url:            ":s3_domain_url",
+
+      styles: { :content => '800>', :thumb => '118x100#' },
+
+      path:           "/:class/:id/:style/:basename.:extension",
+      default_url:    "/:class/:id/:style/:basename.:extension"
 
   validates_attachment_presence :data
   validates_attachment_size :data, :less_than => 100.megabytes
--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -1,8 +1,26 @@
 class Ckeditor::Picture < Ckeditor::Asset
-  has_attached_file :data,
-                    :url  => "/ckeditor_assets/pictures/:id/:style_:basename.:extension",
-                    :path => ":rails_root/public/ckeditor_assets/pictures/:id/:style_:basename.:extension",
-                    :styles => { :content => '800>', :thumb => '118x100#' }
+  # has_attached_file :data,
+  #                   :url  => "/ckeditor_assets/pictures/:id/:style_:basename.:extension",
+  #                   :path => ":rails_root/public/ckeditor_assets/pictures/:id/:style_:basename.:extension",
+  #                   :styles => { :content => '800>', :thumb => '118x100#' }
+    has_attached_file :data,
+
+      s3_credentials: {
+        access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+        bucket:            ENV['S3_BUCKET_NAME']
+      },
+
+      storage:        :s3,
+      s3_headers:     { "Cache-Control" => "max-age=31557600" },
+      s3_protocol:    "https",
+      bucket:         ENV['S3_BUCKET_NAME'],
+      url:            ":s3_domain_url",
+
+      styles: { :content => '800>', :thumb => '118x100#' },
+
+      path:           "/:class/:id/:style/:basename.:extension",
+      default_url:    "/:class/:id/:style/:basename.:extension"
 
   validates_attachment_presence :data
   validates_attachment_size :data, :less_than => 2.megabytes
